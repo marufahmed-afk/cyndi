fc19abda4484c6f387efeee70321bd1744e145f4
Maruf Ahmed <maruf.ahmed@ballys.com>
2026-07-31 17:00:53 +0100

    chore: bump homebrew cask to 0.2.3

--- Cyndi/homebrew-tap/Casks/cyndi.rb
+++ Cyndi/homebrew-tap/Casks/cyndi.rb
@@ -1,6 +1,6 @@
 cask "cyndi" do
-  version "0.2.1"
-  sha256 "2847f3091e290d16252a7d5db6e540d7e62711aee9acfce32b1e28619ec903db"
+  version "0.2.3"
+  sha256 "3a5e7f5caa0efa09bf2dbd24c747d2bddef9a4bd4efba8eff791a013c2df8e1a"
 
   url "https://github.com/marufahmed-afk/cyndi/releases/download/v#{version}/Cyndi.dmg"
   name "Cyndi"
